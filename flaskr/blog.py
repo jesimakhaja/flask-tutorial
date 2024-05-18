6a2fe6a55c474bcf2b5e205f99feff6b80ce7852
Jesima Khaja <jesimakhaja@gmail.com>
2024-05-18 17:54:07 +0530

    blog implementation and related changes in the templates

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,13 +8,17 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
-@bp.route('/')
+@bp.route('/', methods=('GET', 'POST'))
 def index():
+    if request.method == 'POST':
+        keywords = request.form['search']
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' WHERE title like ? or body like ?'
+        ' ORDER BY created DESC',
+        ('%' + keywords + '%', '%' + keywords + '%')
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
